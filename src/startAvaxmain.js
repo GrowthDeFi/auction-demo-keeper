5ce5e05352e42291c359aa5b090ddcdb1f66490b
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-11-08 19:30:50 -0300

    First pass adding Avalanche config

---
+++ src/startAvaxmain.js
@@ -0,0 +1,10 @@
+import Keeper from './keeper.js';
+import path from 'path';
+
+const configPath = path.join(path.resolve(), 'config/avaxmain.json');
+const passwordPath = path.join(path.resolve(), '/wallet/password.txt');
+const keystorePath = path.join(path.resolve(), '/wallet/keystore.json');
+
+const keeper = new Keeper(configPath, passwordPath, keystorePath);
+console.log('\nInitializing Keeper\n');
+keeper.run();
